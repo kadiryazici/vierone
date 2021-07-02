04f42ce55181522867680ee738e80a6ff10e8082
kadiryazici <kyzc411@outlook.com.tr>
2021-07-02 05:26:42 +0300

    updated to 0.3.3

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.toRange = exports.usePromise = exports.inRange = void 0;
+exports.isTruthy = exports.toRange = exports.usePromise = exports.inRange = void 0;
 var inRange_1 = require("./inRange");
 Object.defineProperty(exports, "inRange", { enumerable: true, get: function () { return inRange_1.inRange; } });
 var usePromise_1 = require("./usePromise");
 Object.defineProperty(exports, "usePromise", { enumerable: true, get: function () { return usePromise_1.usePromise; } });
 var toRange_1 = require("./toRange");
 Object.defineProperty(exports, "toRange", { enumerable: true, get: function () { return toRange_1.toRange; } });
+var isTruthy_1 = require("./isTruthy");
+Object.defineProperty(exports, "isTruthy", { enumerable: true, get: function () { return isTruthy_1.isTruthy; } });
